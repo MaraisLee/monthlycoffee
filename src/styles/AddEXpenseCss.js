6997ef9f769a41ddf49a32cd344834cf19bb911c
Yenhj <yen.greact@gmail.com>
2023-02-10 18:06:49 +0900

    utils정리 및 addExpense 작업중

---
+++ src/styles/AddEXpenseCss.js
@@ -0,0 +1,14 @@
+import styled from "@emotion/styled";
+import { txtShadow } from "utils/colors";
+
+export const VioletBt = styled.button`
+  width: 20%;
+  border: 1px solid black;
+  text-align: center;
+  background: #8b8fc8;
+  text-shadow: ${txtShadow};
+  font-size: 20px;
+  color: white;
+  padding: 6px;
+  margin-bottom: 8px;
+`;

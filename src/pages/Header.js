01f90c7d14e09cda0fffc205c1b0a6c6122bab42
Haeparic <v567v202@gmail.com>
2023-02-10 14:52:52 +0900

    사이드바

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -4,7 +4,7 @@ const Header = ({ sidebarOpen, setSidebarOpen }) => {
   const [searchModalOpen, setSearchModalOpen] = useState(false);
 
   return (
-    <header className="sticky top-0 bg-white border-b border-slate-200 z-30">
+    <header className="w-full bg-white border-b border-slate-200">
       <div className="px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16 -mb-px">
           {/* Header: Left side */}
--- src/pages/Sidebar.js
+++ src/pages/Sidebar.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from "react";
+import { NavLink, useLocation } from "react-router-dom";
+
+import SidebarLinkGroup from "./SidebarLinkGruop";
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
-  )
-}
+    <aside className="block w-[20%] p-10 bg-black overflow-y-auto antialiased transition-transform duration-200 -translate-x-full shadow-xl rounded-2xl xl:left-0 xl:translate-x-0">
+      <div className="h-[15%]">
+        <span className="ml-1 text-white font-semibold transition-all duration-200 ease-nav-brand">
+          Argon Dashboard 2
+        </span>
+      </div>
+      <hr className="h-px mt-0 bg-transparent bg-gradient-to-r from-transparent via-black/40 to-transparent dark:bg-gradient-to-r dark:from-transparent dark:via-white dark:to-transparent" />
+      <div>
+        <ul className="text-white">
+          <li>Home</li>
+          <li>Home</li>
+          <li>Home</li>
+          <li>Home</li>
+        </ul>
+      </div>
+    </aside>
+  );
+};
 
-export default Sidebar
+export default Sidebar;

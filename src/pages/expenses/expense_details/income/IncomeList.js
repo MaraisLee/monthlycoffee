b0e96eb01880f0908ece9ef3abdc6ff443fc7956
Haeparic <v567v202@gmail.com>
2023-02-24 18:04:36 +0900

    0224 지출내역 수정중

--- src/pages/expenses/expense_details/income/IncomeList.js
+++ src/pages/expenses/expense_details/income/IncomeList.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import { Close } from "@mui/icons-material";
+import React from "react";
+import axios from "api/axios";
 
-const IncomeList = () => {
+const IncomeList = ({ item, setModalIsOpen }) => {
+  const deleteIncome = () => {
+    if (alert("삭제하시겠습니까?")) {
+      axios
+        .delete(`incomes/${item.id}`)
+        .then((res) => console.log(res))
+        .catch((err) => console.log(err));
+    }
+  };
   return (
-    <div>IncomeList</div>
-  )
-}
+    <div className="relative px-24 h-[13vh] bg-white border border-black">
+      <div className="flex h-full justify-between items-center">
+        <span className="text-xl">{item.date}</span>
+        <div className="flex flex-col items-center">
+          <span className="text-3xl">{item.note}</span>
+        </div>
+        <span className="text-3xl text-green-700">{item.amount}원</span>
+        <span
+          className="absolute right-3 top-3 text-red-800"
+          onClick={deleteIncome}
+        >
+          <Close />
+        </span>
+      </div>
+    </div>
+  );
+};
 
-export default IncomeList
+export default IncomeList;
